9e25a088c1f1a273dc597f945d791c1730fc3d0e
kuzanatoliy <njkznu@gmail.com>
2019-11-10 16:02:41 +0300

    Task7: Fix first version sns functionality

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const http = require('http');
 const express = require('express');
 const AWS = require('aws-sdk');
 const multer = require('multer');
+const bodyParser = require('body-parser');
 
-const { connection, modelList: { Image } } = require('./src/models');
+const { connection, modelList: { Image, Subscription } } = require('./src/models');
 
 AWS.config.loadFromPath('./src/config/aws.config.json');
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 const app = express();
 
 const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
 
 app.get('/random', async (req, res) => {
   try {
@@ -34,9 +38,47 @@ app.get('/:imageName', async (req, res, next) => {
       return next();
     }
     const { fileName } = image.toJSON();
-    console.log(fileName);
     const { ContentType, Body } = await s3.getObject({ Bucket: 'aws.mentoring.program', Key: fileName }).promise();
     res.type(ContentType).send(Body);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/subscribe', async (req, res, next) => {
+  try {
+    if(!req.body.email) {
+      return next();
+    }
+    const { SubscriptionArn } = await sns.subscribe({
+      Protocol: 'email',
+      TopicArn: 'arn:aws:sns:us-east-2:964473949068:AWSMentoringProgramSNSTopic',
+      Endpoint: req.body.email,
+      ReturnSubscriptionArn: true,
+    }).promise();
+    await Subscription.findOrCreate({
+      where: { email: req.body.email, subscription: SubscriptionArn },
+      default: { email: req.body.email, subscription: SubscriptionArn },
+    });
+    res.send('Subscribed');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/unsubscribe', async (req, res, next) => {
+  try {
+    const { subscription } = await Subscription.findOne({
+      where: { email: req.body.email },
+      attributes: [ 'subscription' ],
+    })
+    if(!subscription) {
+      return next();
+    }
+    await sns.unsubscribe({
+      SubscriptionArn: subscription,
+    }).promise();
+    res.send('Unsubscribed');
   } catch (error) {
     res.status(500).send(error);
   }
@@ -46,17 +88,23 @@ const multerMiddleware = multer({ storage: multer.memoryStorage() }).single('ima
 
 app.post('/', multerMiddleware, async (req, res) => {
   try {
-    const { originalname, buffer } = req.file;
+    const { originalname, buffer, mimetype } = req.file;
     await s3.upload({
       ACL: 'public-read',
       Bucket: 'aws.mentoring.program',
       Key: originalname,
       Body: buffer,
+      ContentType: mimetype,
     }).promise();
-    Image.findOrCreate({
+    await Image.findOrCreate({
       where: { fileName: originalname },
       default: { fileName: originalname },
     });
+    result = await sns.publish({
+      Subject: 'AwsMentoringProgram: new image',
+      Message: `${ req.protocol }://${req.hostname }/${originalname}`,
+      TargetArn: 'arn:aws:sns:us-east-2:964473949068:AWSMentoringProgramSNSTopic',
+    }).promise();
     res.status(200).send('File was saved');
   } catch (error) {
     res.status(500).send(error);
--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 
 const { defineImageModel } = require('./image.model');
+const { defineSubscriptionModel } = require('./subscription.model');
 const operatorAliases = require('./operatorAliases');
 const config = require('../config/database.config');
 
 const connection = new Sequelize({ ...config.development, operatorAliases });
 const modelList = {
   Image: defineImageModel(connection, Sequelize),
+  Subscription: defineSubscriptionModel(connection, Sequelize),
 }
 
 module.exports = {
